dc213908f76336aeb0496c6046eddd1d76e58d7c
Roy2055 <105153567+Roy2055@users.noreply.github.com>
2022-07-19 15:28:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace MyTest
+{
+    public class Program : ValidateWord
+
+    {
+        static async Task Main(string[] args)
+        {
+            ///Dpwn load from internet.
+            string filePath = @"https://raw.githubusercontent.com/Roy2055/RepoTest/main/SampleFile.txt";
+            HttpClient httpClient = new();
+            var content = await httpClient.GetStringAsync(filePath);
+           
+            string[] lines = content.Split(
+                new string[] { Environment.NewLine }, StringSplitOptions.None);
+               
+            string finalText = "";
+            foreach (string line in lines)
+            {
+                StringBuilder sb = new();
+                string[] allWords = Regex.Split(line, @" ");
+
+                for (int i = 0; i < allWords.Length; i++) 
+                {
+                    sb.Append(ValidWord(allWords[i]));
+                }
+               ///File ouptut
+                finalText += sb.ToString() + "\n";
+            }
+            using (System.IO.StreamWriter file = new(@"c:\testFileCleaned.txt"))
+            {
+                ///Write file to disk
+                file.Write(finalText);
+            }  
+        }
+    }
+}
---
+++ ValidateWord.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyTest
+{
+    public class ValidateWord
+    {
+
+        public static string ValidWord(string inputWord)
+        {
+            string originalWord = inputWord;
+
+            if (inputWord.Length < 3)
+            {
+                return ""; //too small
+            }
+
+            StringComparison c = StringComparison.OrdinalIgnoreCase;
+            if (inputWord.Contains('t', c))
+            {
+                return "";  //contains invalid letter
+            }
+
+            string cleanWord = Regex.Replace(inputWord, @"[^A-Za-z]+", String.Empty);
+
+            int offset = cleanWord.Length / 2;
+
+            if (cleanWord.Length % 2 == 0)
+            {
+                //Even word
+                string newString = cleanWord[offset - 1].ToString() + cleanWord[offset].ToString();
+
+                if (Regex.Match(newString, @"[aeiou]").Length > 0)
+                {
+                    return "";  //found vowels
+                }
+                else
+                {
+                    return " " + originalWord; //No Vowels
+                }
+            }
+            else
+            {
+                //Console.WriteLine("The word is Fraction");
+                if (Regex.Match(cleanWord[offset].ToString(), @"[^aeiou]").Length > 0)
+                {
+                    return " " + originalWord; //Found Vowel(s)
+                }
+                else
+                {
+                    return ""; //No Vowels
+                }
+            }
+        }
+    }
+}
