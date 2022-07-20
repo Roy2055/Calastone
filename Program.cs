2b02f47fc4372d9afb1975e9476526e193ec6191
Roy2055 <105153567+Roy2055@users.noreply.github.com>
2022-07-20 09:28:14 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,9 +7,20 @@ namespace MyTest
     public class Program : ValidateWord
 
     {
+        /// <summary>
+        /// Validates a text file from github
+        /// Eliminates words from the file based on the following
+        /// criteria:
+        /// 1. The has a vowel as the middle character(s);
+        /// 2. The word contains a 't';
+        /// 3. The word is less than 3 charaters.
+        /// class <c>ValidWord</c> is used to ascertain if the word is valid.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         static async Task Main(string[] args)
         {
-            ///Dpwn load from internet.
+            ///Download from internet.
             string filePath = @"https://raw.githubusercontent.com/Roy2055/RepoTest/main/SampleFile.txt";
             HttpClient httpClient = new();
             var content = await httpClient.GetStringAsync(filePath);
@@ -25,14 +36,16 @@ namespace MyTest
 
                 for (int i = 0; i < allWords.Length; i++) 
                 {
+                    
                     sb.Append(ValidWord(allWords[i]));
                 }
-               ///File ouptut
+                ///File ouptut
                 finalText += sb.ToString() + "\n";
             }
             using (System.IO.StreamWriter file = new(@"c:\testFileCleaned.txt"))
             {
-                ///Write file to disk
+                ///Write file to local disk
+                Console.WriteLine("File cleansing completed.");
                 file.Write(finalText);
             }  
         }
